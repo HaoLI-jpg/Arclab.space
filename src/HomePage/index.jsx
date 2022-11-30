bc8310afd96baefadfaa40583d7c1554da011056
“Jiaying <jzhe1999@gmail.com>
2022-11-30 18:37:16 +1300

    completed user sign in/sign up pages

--- src/HomePage/index.jsx
+++ src/HomePage/index.jsx
@@ -1,55 +1,31 @@
 import React, { useState } from 'react';
 import styles from './style.module.scss';
-import { Row, Col, Button } from 'antd';
-import UserLogin from '../UserLogin';
-import {
-  UserOutlined
-} from '@ant-design/icons';
+import UserSignInAndSignUp from '../UserSignInAndSignUp';
+import NavBar from '../NavBar';
+import { Button } from 'antd';
 
 function HomePage() {
   const [openModal, setOpenModal] = useState(false);
+
   return (
     <div className={styles.homeBackground}>
-      {/* Navbar */}
-      <div className={styles.navBarBox}>
-        <Row>
-          <Col span={4}>
-            <div className={styles.circleLogo}>
-              <img src="https://media-exp1.licdn.com/dms/image/C560BAQE5iwUWPs6fFQ/company-logo_200_200/0/1556248126796?e=1677715200&v=beta&t=PTwDzm1Q7IvVjJyTnXcOOFULThRN4l6rR8X3-jxVjMw" alt="Arclab Logo" className={styles.logo} />
-            </div>
-          </Col>
-          <Col span={17}></Col>
-          <Col span={2}>
-            <div className={styles.aboutIconBox}>
-              <Button className={styles.aboutIconButton}>About</Button>
-            </div>
-          </Col>
-          <Col span={1}>
-            <div className={styles.userIconBox}>
-              <Button
-                className={styles.userIconButton}
-                onClick={() => {
-                  setOpenModal(true);
-                }}
-              >
-                <UserOutlined />
-              </Button>
-            </div>
-          </Col>
-        </Row>
-
-      </div>
+      {/* NavBar */}
+      <NavBar />
 
       {/* Body */}
       {/* Company Info */}
-      <div className={styles.companyInfoBox}>
-        <div className={styles.companyInfo}>
-          Company information(eg. Reasons for choosing this company)
+      <div className={styles.companyInfoOuter}>
+        <div className={styles.companyInfoBox}>
+          <div className={styles.companyInfo}>
+            Company information(eg. Reasons for choosing this company)
+          </div>
+          <div className={styles.joinBtnBox}>
+            <Button className={styles.joinBtn} onClick={() => { setOpenModal(true) }}>Join us now</Button>
+          </div>
         </div>
-        <div className={styles.joinBtnBox}>
-          <Button className={styles.joinBtn}>Join us now</Button>
-        </div>
+
       </div>
+
 
       {/* An example video of deliverables */}
       <div className={styles.exampleVideoBox}>
@@ -67,12 +43,12 @@ function HomePage() {
 
       {
         openModal && (
-          <UserLogin 
+          <UserSignInAndSignUp
             setOpenModal={setOpenModal}
+            pageType={'register'}
           />
         )
       }
-
     </div>
 
   )
---
+++ src/UserSignInAndSignUp/index.jsx
@@ -0,0 +1,155 @@
+import React, { useState } from 'react';
+import styles from './style.module.scss';
+import { LockOutlined, UserOutlined } from '@ant-design/icons';
+import { Button, Checkbox, Form, Input } from 'antd';
+import {
+  CloseOutlined
+} from '@ant-design/icons';
+
+export default function UserSignInAndSignUp({ setOpenModal, pageType = 'login' }) {
+  const [page, setPage] = useState(pageType);
+  return (
+    <div className={styles.shadow}>
+      <div className={styles.userLoginDialog}>
+        <div className={styles.imageBox}>
+          <Button
+            className={styles.closeBtn}
+            onClick={() => {
+              setOpenModal(false)
+            }}
+          ><CloseOutlined /></Button>
+        </div>
+        <div className={styles.userLoginBox}>
+          <div className={styles.userLogin}>
+            {
+              page === 'login' && (
+                <div>
+                  <div className={styles.textBox}>
+                    <h1>Welcome Back</h1>
+                    <p>Please Log in</p>
+                  </div>
+                  <div className={styles.userDetails}>
+                    <Form
+                      name="normal_login"
+                      className="login-form"
+                      initialValues={{
+                        remember: true,
+                      }}
+                    >
+                      <Form.Item
+                        name="username"
+                        rules={[
+                          {
+                            required: true,
+                            message: 'Please input your Username!',
+                          },
+                        ]}
+                      >
+                        <Input className={styles.usernameInput} prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
+                      </Form.Item>
+                      <Form.Item
+                        name="password"
+                        rules={[
+                          {
+                            required: true,
+                            message: 'Please input your Password!',
+                          },
+                        ]}
+                      >
+                        <Input className={styles.pwdInput}
+                          prefix={<LockOutlined className="site-form-item-icon" />}
+                          type="password"
+                          placeholder="Password"
+                        />
+                      </Form.Item>
+                      <Form.Item>
+                        <Form.Item name="remember" valuePropName="checked" noStyle>
+                          <Checkbox className={styles.rememberMe}>Remember me</Checkbox>
+                        </Form.Item>
+
+                        <a className={styles.forgotPwdLink} href=''>
+                          Forgot password
+                        </a>
+                        </Form.Item>
+                        <Form.Item>
+                        <a className={styles.registerLink} onClick={() => {
+                          setPage('register')
+                        }}>Register now</a>
+                      </Form.Item>
+                      <div className={styles.loginBtnBox}>
+                        <Form.Item>
+                          <Button className={styles.loginBtn} type="primary" htmlType="submit" >
+                            Sign in
+                          </Button>
+                        </Form.Item>
+                      </div>
+                    </Form>
+                  </div>
+                </div>
+              )
+            }
+            {
+              page === 'register' && (
+                <div>
+                  <div className={styles.textBox}>
+                    <h1>Welcome</h1>
+                    <p>Please Sign up</p>
+                  </div>
+                  <div className={styles.userDetails}>
+                    <Form
+                      name="normal_login"
+                      className="login-form"
+                      initialValues={{
+                        remember: true,
+                      }}
+                    >
+                      <Form.Item
+                        name="username"
+                        rules={[
+                          {
+                            required: true,
+                            message: 'Please input your Username!',
+                          },
+                        ]}
+                      >
+                        <Input className={styles.usernameInput} prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
+                      </Form.Item>
+                      <Form.Item
+                        name="password"
+                        rules={[
+                          {
+                            required: true,
+                            message: 'Please input your Password!',
+                          },
+                        ]}
+                      >
+                        <Input className={styles.pwdInput}
+                          prefix={<LockOutlined className="site-form-item-icon" />}
+                          type="password"
+                          placeholder="Password"
+                        />
+                      </Form.Item>
+                      <Form.Item>
+                        <a className={styles.loginLink} onClick={()=>{setPage('login')}}>Login Here</a>
+                      </Form.Item>
+                      <div className={styles.signUpBtnBox}>
+                        <Form.Item>
+                          <Button className={styles.signUpBtn} type="primary" htmlType="submit" >
+                            Sign up
+                          </Button>
+                        </Form.Item>
+                      </div>
+
+                    </Form>
+                  </div>
+                </div>
+              )
+            }
+          </div>
+        </div>
+
+      </div>
+    </div>
+
+  )
+}
