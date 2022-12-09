c52d1e4736b12ae8a323fa76a2771de7adf97cab
“Jiaying <jzhe1999@gmail.com>
2022-12-09 21:48:18 +1300

    backToHome Button

--- src/ProductPage/index.jsx
+++ src/ProductPage/index.jsx
@@ -4,47 +4,58 @@ import Collapsible from 'react-collapsible';
 import styles from './style.module.scss'
 import { PageInfo } from '../Components/PageInfo';
 import { PointCloudDisplay } from '../Components/PointCloudDisplay';
+import { useNavigate } from 'react-router-dom'
+import { Button } from 'antd';
 
 const ProductPage = () => {
+    const navigate = useNavigate()
     return (
         <>
-        <NavBar />
-        <div className={styles.container}>
-            <PageInfo />
 
-            <h4>
-            <Collapsible trigger="+ POINT CLOUD" className={styles.collapsible}>
-                <h4 className={styles.Collapsible__contentInner}>
-                    + LiDAR
-                </h4>
-                <h4 className={styles.Collapsible__contentInner}>
-                    + Drone
-                </h4>
-            </Collapsible>
-            </h4>
-            <h4>
-            <Collapsible trigger="+ INTERNAL WALKTHROUGH" className={styles.collapsible}>
-                <h4 className={styles.Collapsible__contentInner}>
-                    + LiDAR
-                </h4>
-                <h4 className={styles.Collapsible__contentInner}>
-                    + Drone
-                </h4>
-            </Collapsible>
-            </h4>
-            <h4>
-            <Collapsible trigger="+ CAD SURVEY PLAN" className={styles.collapsible}>
-                <h4 className={styles.Collapsible__contentInner}>
-                    + LiDAR
-                </h4>
-                <h4 className={styles.Collapsible__contentInner}>
-                    + Drone
-                </h4>
-            </Collapsible>
-            </h4>
+            <NavBar />
+            <div className={styles.container}>
+                <PageInfo />
+                <Button className={styles.backToHome}
+                    onClick={() => {
+                        navigate('/')
+                    }}
+                >
+                    Back
+                </Button>
 
-            <PointCloudDisplay />
-        </div>
+                <h4>
+                    <Collapsible trigger="+ POINT CLOUD" className={styles.collapsible}>
+                        <h4 className={styles.Collapsible__contentInner}>
+                            + LiDAR
+                        </h4>
+                        <h4 className={styles.Collapsible__contentInner}>
+                            + Drone
+                        </h4>
+                    </Collapsible>
+                </h4>
+                <h4>
+                    <Collapsible trigger="+ INTERNAL WALKTHROUGH" className={styles.collapsible}>
+                        <h4 className={styles.Collapsible__contentInner}>
+                            + LiDAR
+                        </h4>
+                        <h4 className={styles.Collapsible__contentInner}>
+                            + Drone
+                        </h4>
+                    </Collapsible>
+                </h4>
+                <h4>
+                    <Collapsible trigger="+ CAD SURVEY PLAN" className={styles.collapsible}>
+                        <h4 className={styles.Collapsible__contentInner}>
+                            + LiDAR
+                        </h4>
+                        <h4 className={styles.Collapsible__contentInner}>
+                            + Drone
+                        </h4>
+                    </Collapsible>
+                </h4>
+
+                <PointCloudDisplay />
+            </div>
         </>
     )
 }
